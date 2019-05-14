f23cc4c3ecea317bdf35c8937e3b0438c39555d9
amandabacon <amandabacon@bennington.edu>
2019-05-14 09:58:48 -0400

    update pinout.py

--- pinout.py
+++ pinout.py
@@ -6,21 +6,21 @@
 #--------------------------------------------------------
 
 #Relays (6 total) Require GND,VCC,CNTRL
-#Two for hardware safety
+#Two for hardware safety--one GPIO pin together
 GPIO_pin_relays = [8,10,12,16,18,22]
 
 #LED buttons (4 total) Require GND,VCC (3.3 V)
-#Backward--
-#Forward--
-#Home--
-#E-Stop--
+#Backward(cc)--13
+#Forward(c)--15
+#Home--11
+#E-Stop--19
 GPIO_pin_buttons = [11,13,15,19]
 
 #IR Beam Break Sensor (2 total) Require GND,VCC,GPIO (signal)
 #Object needs to block IR light (metal)
 #Transmittor (black) has 2 wires (GND,VCC)
 #Receiver (clear) has 3 wires (signal,GND,VCC)
-#Need 10 K Ohm resistor
+#Need 10 K Ohm resistor (only if not using pull_up_down)
 #One for position location
 #One for home location
 GPIO_pin_IR = [7,21]
